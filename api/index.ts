6937a757618d7c51db14845b4c9bee233db24023
HusseinSerag <117987162+HusseinSerag@users.noreply.github.com>
2024-07-01 16:59:35 +0300

    remove helmet

--- api/index.ts
+++ api/index.ts
@@ -13,14 +13,14 @@ import "./utils/cloudinary";
 
 const app = express();
 
-app.use(
-  helmet.contentSecurityPolicy({
-    useDefaults: true,
-    directives: {
-      "img-src": ["'self'", "https: data:"],
-    },
-  })
-);
+// app.use(
+//   helmet.contentSecurityPolicy({
+//     useDefaults: true,
+//     directives: {
+//       "img-src": ["'self'", "https: data:"],
+//     },
+//   })
+// );
 app.use(
   cors({
     credentials: true,
